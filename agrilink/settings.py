00b9e7ec0ecbc2be9c9cc13b87c5e341ac70872a
konditi1 <konditi1@gmail.com>
2025-03-10 23:31:19 +0300

    fix: add whitenoise to handle static files

--- agrilink/settings.py
+++ agrilink/settings.py
@@ -124,7 +124,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 
-    'whitenoise.middleware.WhiteNoiseMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'corsheaders.middleware.CorsMiddleware',    
 ]
 
